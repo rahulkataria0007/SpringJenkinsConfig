e65b55fa4222fbbf5134825255d824f458fd82b7
Rahul Kataria <rk00640089@techmahindra.com>
2020-06-21 10:51:47 +0530

    REmove unused annotations

--- src/main/java/com/example/SpringJenkinBuild/beans/User.java
+++ src/main/java/com/example/SpringJenkinBuild/beans/User.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import lombok.NonNull;
 
 public class User {
 
